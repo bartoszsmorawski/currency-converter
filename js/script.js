9166c61d08b53c35ead77201cce34c85684cd442
Bartosz Smorawski <holzturbs@gmail.com>
2023-04-18 09:02:22 +0200

    added js-function (organize)

--- js/script.js
+++ js/script.js
@@ -1,179 +1,198 @@
-let formElement = document.querySelector(".js-form");
-let amountElement = document.querySelector(".js-amount");
-let currencyElement = document.querySelector(".js-currency");
-let resultElement = document.querySelector(".js-result");
+{
+    const calculateResult = (amount, currency) => {
+        const rateTHB = 0.1294;
+        const rateUSD = 4.4130;
+        const rateAUD = 2.9466;
+        const rateHKD = 0.5626;
+        const rateCAD = 3.2168;
+        const rateNZD = 2.7521;
+        const rateSGD = 3.2890;
+        const rateEUR = 4.7109;
+        const rateHUF = 0.0118;
+        const rateCHF = 4.7477;
+        const rateGBP = 5.3808;
+        const rateUAH = 0.1258;
+        const rateJPY = 3.3634;
+        const rateCZK = 0.1963;
+        const rateDKK = 0.6328;
+        const rateISK = 3.1469;
+        const rateNOK = 0.4119;
+        const rateSEK = 0.4205;
+        const rateRON = 0.9572;
+        const rateBGN = 2.4086;
+        const rateTRY = 0.2322;
+        const rateILS = 1.2003;
+        const rateCLP = 0.0053;
+        const ratePHP = 0.0809;
+        const rateMXN = 0.2321;
+        const rateZAR = 0.2384;
+        const rateBRL = 0.8365;
+        const rateMYR = 0.9837;
+        const rateIDR = 0.0002;
+        const rateINR = 0.0534;
+        const rateKRW = 0.0033;
+        const rateCNY = 0.6408;
 
-let rateTHB = 0.1294;
-let rateUSD = 4.4130;
-let rateAUD = 2.9466;
-let rateHKD = 0.5626;
-let rateCAD = 3.2168;
-let rateNZD = 2.7521;
-let rateSGD = 3.2890;
-let rateEUR = 4.7109;
-let rateHUF = 0.0118;
-let rateCHF = 4.7477;
-let rateGBP = 5.3808;
-let rateUAH = 0.1258;
-let rateJPY = 3.3634;
-let rateCZK = 0.1963;
-let rateDKK = 0.6328;
-let rateISK = 3.1469;
-let rateNOK = 0.4119;
-let rateSEK = 0.4205;
-let rateRON = 0.9572;
-let rateBGN = 2.4086;
-let rateTRY = 0.2322;
-let rateILS = 1.2003;
-let rateCLP = 0.0053;
-let ratePHP = 0.0809;
-let rateMXN = 0.2321;
-let rateZAR = 0.2384;
-let rateBRL = 0.8365;
-let rateMYR = 0.9837;
-let rateIDR = 0.0002;
-let rateINR = 0.0534;
-let rateKRW = 0.0033;
-let rateCNY = 0.6408;
+        switch (currency) {
+            case "THB":
+                return amount / rateTHB;
 
-formElement.addEventListener("submit", (event) => {
-    event.preventDefault();
 
-    let amount = +amountElement.value;
-    let currency = currencyElement.value;
+            case "USD":
+                return amount / rateUSD;
 
-    let result;
 
-    switch (currency) {
-        case "THB":
-            result = amount / rateTHB;
-            break;
+            case "AUD":
+                return amount / rateAUD;
 
-        case "USD":
-            result = amount / rateUSD;
-            break;
 
-        case "AUD":
-            result = amount / rateAUD;
-            break;
+            case "HKD":
+                return amount / rateHKD;
 
-        case "HKD":
-            result = amount / rateHKD;
-            break;
 
-        case "CAD":
-            result = amount / rateCAD;
-            break;
+            case "CAD":
+                return amount / rateCAD;
 
-        case "NZD":
-            result = amount / rateNZD;
-            break;
 
-        case "SGD":
-            result = amount / rateSGD;
-            break;
+            case "NZD":
+                return amount / rateNZD;
 
-        case "EUR":
-            result = amount / rateEUR;
-            break;
 
-        case "HUF":
-            result = amount / rateHUF;
-            break;
+            case "SGD":
+                return amount / rateSGD;
 
-        case "CHF":
-            result = amount / rateCHF;
-            break;
 
-        case "GBP":
-            result = amount / rateGBP;
-            break;
+            case "EUR":
+                return amount / rateEUR;
 
-        case "UAH":
-            result = amount / rateUAH;
-            break;
 
-        case "JPY":
-            result = amount / rateJPY;
-            break;
+            case "HUF":
+                return amount / rateHUF;
 
-        case "CZK":
-            result = amount / rateCZK;
-            break;
 
-        case "DKK":
-            result = amount / rateDKK;
-            break;
+            case "CHF":
+                return amount / rateCHF;
 
-        case "ISK":
-            result = amount / rateISK;
-            break;
 
-        case "NOK":
-            result = amount / rateNOK;
-            break;
+            case "GBP":
+                return amount / rateGBP;
 
-        case "SEK":
-            result = amount / rateSEK;
-            break;
 
-        case "RON":
-            result = amount / rateRON;
-            break;
+            case "UAH":
+                return amount / rateUAH;
 
-        case "BGN":
-            result = amount / rateBGN;
-            break;
 
-        case "TRY":
-            result = amount / rateTRY;
-            break;
+            case "JPY":
+                return amount / rateJPY;
 
-        case "ILS":
-            result = amount / rateILS;
-            break;
 
-        case "CLP":
-            result = amount / rateCLP;
-            break;
+            case "CZK":
+                return amount / rateCZK;
 
-        case "PHP":
-            result = amount / ratePHP;
-            break;
 
-        case "MXN":
-            result = amount / rateMXN;
-            break;
+            case "DKK":
+                return amount / rateDKK;
 
-        case "ZAR":
-            result = amount / rateZAR;
-            break;
 
-        case "BRL":
-            result = amount / rateBRL;
-            break;
+            case "ISK":
+                return amount / rateISK;
 
-        case "MYR":
-            result = amount / rateMYR;
-            break;
 
-        case "IDR":
-            result = amount / rateIDR;
-            break;
+            case "NOK":
+                return amount / rateNOK;
 
-        case "INR":
-            result = amount / rateINR;
-            break;
 
-        case "KRW":
-            result = amount / rateKRW;
-            break;
+            case "SEK":
+                return amount / rateSEK;
 
-        case "CNY":
-            result = amount / rateCNY;
-            break;
 
+            case "RON":
+                return amount / rateRON;
+
+
+            case "BGN":
+                return amount / rateBGN;
+
+
+            case "TRY":
+                return amount / rateTRY;
+
+
+            case "ILS":
+                return amount / rateILS;
+
+
+            case "CLP":
+                return amount / rateCLP;
+
+
+            case "PHP":
+                return amount / ratePHP;
+
+
+            case "MXN":
+                return amount / rateMXN;
+
+
+            case "ZAR":
+                return amount / rateZAR;
+
+
+            case "BRL":
+                return amount / rateBRL;
+
+
+            case "MYR":
+                return amount / rateMYR;
+
+
+            case "IDR":
+                return amount / rateIDR;
+
+
+            case "INR":
+                return amount / rateINR;
+
+
+            case "KRW":
+                return amount / rateKRW;
+
+
+            case "CNY":
+                return amount / rateCNY;
+
+
+        }
+    };
+
+    const updateResultText = (amount, result, currency) => {
+        const resultElement = document.querySelector(".js-result");
+        resultElement.innerHTML = `${amount.toFixed(2)} PLN = <strong>${result.toFixed(2)} ${currency}</strong>`;
     }
+    
 
-    resultElement.innerHTML = `${amount.toFixed(2)} PLN = <strong>${result.toFixed(2)} ${currency}</strong>`;
-}); 
+    const onFormSubmit = (event) => {
+        event.preventDefault();
+
+
+        const amountElement = document.querySelector(".js-amount");
+        const currencyElement = document.querySelector(".js-currency");
+        
+
+        const amount = +amountElement.value;
+        const currency = currencyElement.value;
+
+        const result = calculateResult(amount, currency);
+
+        updateResultText (amount, result, currency);
+    };
+
+    const init = () => {
+        const formElement = document.querySelector(".js-form");
+
+        formElement.addEventListener("submit", onFormSubmit);
+    };
+
+    init();
+
+}
